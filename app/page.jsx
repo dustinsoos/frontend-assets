09e399e23ffffd781cd46d5c42443ccd640e1803
Dustin Soos <dustinmsoos@gmail.com>
2023-08-14 11:56:35 -0400

    First Commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className=" h-screen bg-slate-900 text-white">Hello</div>
+    </main>
+  );
+}
