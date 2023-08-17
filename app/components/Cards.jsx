7ecb8c35d3f8cb1f2ab16d0086d7b615ae954006
Dustin Soos <dustinmsoos@gmail.com>
2023-08-17 14:34:43 -0400

    laptop version view change

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -33,8 +33,8 @@ export default function Cards() {
 
   return (
     <>
-      <section className="h-screen px-2 md:px-16 lg:px-32 xl:px-64 bg-slate-900 grid grid-cols-1 ">
-        <h3 className=" text-sky-500 text-3xl font-semibold pt-8   md:pt-8  md:mb-0  lg:pt-64">
+      <section className="h-screen px-2 md:px-16 lg:px-32 xl:px-64 bg-slate-900 grid grid-cols-1 md:block md:grid-cols-none">
+        <h3 className=" grid text-sky-500 text-3xl items-center font-semibold pt-8  md:pt-8  md:mb-0  lg:pt-64">
           What do we offer?
         </h3>
         <h4 className=" hidden mb-4 md:pb-8  text-white text-2xl md:block">
